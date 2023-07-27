3c3dc82980cd0aa33fe9aa7a7ce1c9f53ea93703
azishio <azishio3141@gmail.com>
2023-07-28 00:33:08 +0900

    keyマネージャーの作成

---
+++ src/Editor/Manager/KeyManager.ts
@@ -0,0 +1,31 @@
+export default class KeyManager {
+  private static usedKeys = new Set<string>();
+
+  static genKey() {
+    const { usedKeys } = KeyManager;
+
+    const charset =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const charsetLength = charset.length;
+    const randomStr = () => {
+      let str = "";
+      for (let i = 0; i <= 3; i++) {
+        const index = Math.floor(Math.random() * charsetLength);
+        str += charset.charAt(index);
+      }
+      return str;
+    };
+
+    let key = randomStr();
+    while (usedKeys.has(key)) {
+      key = randomStr();
+    }
+
+    usedKeys.add(key);
+    return key;
+  }
+
+  static unregister(key: string) {
+    KeyManager.usedKeys.delete(key);
+  }
+}
